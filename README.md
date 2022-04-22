1c5d8a6d8df0b47250d7402367e0befd28b8c2a4
henapaartae123 <henyvibe@gmail.com>
2022-04-22 13:51:11 +0300

    yes

---
+++ README.md
@@ -0,0 +1,27 @@
+# _printf
+
+A re-creation of the printf function in C!
+
+
+## Requirements for this project
+
+- Code must comply with the [Betty](https://github.com/holbertonschool/Betty) style and document checks.
+- Compile code using flags: `-Wall`, `-Werror`, `-Wextra`, `-pedantic`, `-Wno-format`
+- Cannot use global variables.
+- Restricted to no more than 5 functions per file.
+- Function prototypes should be included in header file `main.h`
+- Header files should be include guarded.
+- Authorized functions and macros:
+..* `write` (`man 2 write`)
+..* `malloc` (`man 3 malloc`)
+..* `free` (`man 3 free`)
+..* `va_start` (`man 3 va_start`)
+..* `va_end` (`man 3 va_end`)
+..* `va_copy` (`man 3 va_copy`)
+..* `va_arg` (`man 3 va_arg`)
+
+## Team
+
+*Okoli Ekene* - [Github](https://github.com/khennyyb) || [Twitter](https://twitter.com/khennyyofficial)
+
+*Nkiruka Ebele* - [Github](https://github.com/nkirukka) 
--- _printf.c
+++ _printf.c
@@ -1,65 +1,37 @@
-/* Online C compiler to run C program online */
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * _printf - function that produces output according to a format.
- * @format: pointer to character string composed of 0 or more directives.
- *
- * write output to stdout, the standard output stream.
- * You need to handle the following conversion specifiers: c, s, %.
- * You don’t have to reproduce the buffer handling of the C library printf,
- * function.
- * You don’t have to handle the flag characters.
- * You don’t have to handle field width.
- * You don’t have to handle precision.
- * You don’t have to handle the length modifiers.
- *
- * Return: the number of characters printed excluding the null byte,
- * used to end output strings.
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	if (format == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
